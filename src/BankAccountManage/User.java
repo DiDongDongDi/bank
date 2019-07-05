0a80cf7efc5accf55c32aa277716951fdc908a7d
DiDongDongDi <2572931653@qq.com>
2019-07-05 10:26:59 +0800

    delete ./.idea

---
+++ src/BankAccountManage/User.java
@@ -0,0 +1,23 @@
+public class User{
+	String name;
+	String password;
+
+	User(){
+		
+	}
+
+	User(String name, String password){
+		this.name=name;
+		this.password=password;
+	}
+
+	void setName(String name){
+		this.name=name;
+	}
+
+	void setPassword(String password){
+		this.password=password();
+	}
+
+	
+}
