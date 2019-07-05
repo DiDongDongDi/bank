82314358b2f891ff0279749c112a73008e1e2de9
zhangzhaoxuan <1040517659@qq.com>
2019-07-05 08:59:56 +0800

    基类
    123

---
+++ src/Card.java
@@ -0,0 +1,21 @@
+public class Card {
+    private String CardNumber;
+    private String code;
+
+    public void setCardNumber(String cardNumber) {
+        CardNumber = cardNumber;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCardNumber() {
+        return CardNumber;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    
+}
